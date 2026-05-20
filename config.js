581df00e46dd18cb824453609d0d4e1a0539099e
Knight <42996469+knight-mao@users.noreply.github.com>
2026-05-20 15:32:32 +0800

    Set Supabase config

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 window.LABDM_SUPABASE = {
   // Fill these with your Supabase Project URL and anon publishable key.
   // Never put a service_role key in this browser-side file.
-  url: "",
-  anonKey: ""
+  url: "https://xhlyewajwarlxvnrkdrh.supabase.co",
+  anonKey: "sb_publishable_15UxhmGN-2elMxanakXtwg_9X5XotBU"
 };
